07dacb2d19ccd2c84840307dbd2c2fae09cd2267
Igor Tatarintsev <ietatarin@gmail.com>
2026-01-21 12:30:04 +0100

    Add i3blocks

---
+++ i3blocks/.config/i3blocks/scripts/currentlyPlaying.js
@@ -0,0 +1,19 @@
+const output = ({ full_text, color = '#333333' }) => {
+  console.log(JSON.stringify({ full_text, color }))
+}
+
+fetch('http://websockets.pickrelated.com/history?id=ietatarin@gmail.com').then(
+  (response) => {
+    response.json().then((json) => {
+      const songs = json.filter(({ event }) => event === 'song') || []
+      const song = songs.pop()?.payload || {}
+      if (!song.playbackStatus) {
+        output({ full_text: '  ', color: '#0a0a0a' })
+        return
+      }
+      const likeIcon = song.isLiked === 'true' ? ' ' : ''
+      const songText = `${song.artist} - ${song.track}${likeIcon}`
+      output({ full_text: songText })
+    })
+  },
+)
